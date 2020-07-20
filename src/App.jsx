b7ab3150fdb2fadcb4c1f8a9d319ecf70a55da98
jason <Jason.jafari.m@gmail.com>
2020-07-19 23:16:42 -0400

    get fetch data into state

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 // https://orangevalleycaa.org/api/videos
 function App() {
-  const [data,setData]=useState([])
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://orangevalleycaa.org/api/videos")
+        .then(response => response.json())
+        .catch(e => console.log(e))
+      console.log(result);
+      setData(result)
+    }
+    fetchData()
+  } ,[]  )
+
   return (
     <div className="App">
       <header>

4115f08d1aa2c4063364ae9259cda7f440d3f450
jason <Jason.jafari.m@gmail.com>
2020-07-19 23:09:09 -0400

    handeliing fetch data

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+// https://orangevalleycaa.org/api/videos
+function App() {
+  const [data,setData]=useState([])
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          Videos
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
